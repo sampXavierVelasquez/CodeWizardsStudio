6cf847cc96c4bc5fa7dd6b89574afc67e30b7aa3
sampXavierVelasquez <146622526+sampXavierVelasquez@users.noreply.github.com>
2024-07-07 01:13:24 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-function showSection(sectionId) {
-    // Скрываем все секции
-    const sections = document.querySelectorAll('.section');
-    sections.forEach(section => {
-        section.classList.remove('active');
-    });
-
-    // Показываем выбранную секцию
-    const activeSection = document.getElementById(sectionId);
-    if (activeSection) {
-        activeSection.classList.add('active');
-    }
-}
-
 function openModal(modalId) {
     const modal = document.getElementById(modalId);
     if (modal) {
